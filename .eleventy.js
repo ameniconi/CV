b56aebdb73f82bdbfa708edd742be847e97c5ca3
ameniconi <angelo.meniconi@gmail.com>
2026-02-21 14:04:42 +0000

    fixing path

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,6 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: { input: ".", includes: "_includes", output: "_site" },
-    pathPrefix: "/gh-pages/"  // <-- your GitHub repo name
+    pathPrefix: "/gh-pages"  // <-- replace with your GitHub repo name
   };
 };
